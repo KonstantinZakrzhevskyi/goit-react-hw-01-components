4a53da36b3e44f6891d97193aa4d2e2ff0198dba
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2021-10-30 00:03:33 +0300

    Добавляет задание 1 и 2 без стилизации

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+// import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+import Profile from 'components/Profile';
+import user from './data/user.json';
+
+import Statistics from 'components/Statistics';
+import statisticalData from 'data/statistical-data.json';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics stats={statisticalData} />;
+    </div>
+  );
 }
-
-export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import defaultImage from 'images/defaultAvatar.png';
+
+export default function Profile({
+  name,
+  tag,
+  location,
+  avatar = defaultImage,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt={name} className="avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
